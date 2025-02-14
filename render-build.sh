a4b341743b89677af600e51848f2c79fcaa40a0a
prince1gurjar <49690988+prince1gurjar@users.noreply.github.com>
2025-02-14 22:06:25 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -9,6 +9,9 @@ apt-get install -y google-chrome-stable
 # Install ChromeDriver
 apt-get install -y chromium-chromedriver
 
+# Set Permissions
+chmod +x /usr/lib/chromium-browser/chromedriver
+
 # Show versions (for debugging)
 google-chrome --version
 chromedriver --version
