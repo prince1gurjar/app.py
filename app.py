346b5e9805ea661c56b727c4934d6d46ae814022
prince1gurjar <49690988+prince1gurjar@users.noreply.github.com>
2025-02-14 19:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,7 @@ def invite():
     return jsonify({"message": result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+app.run(host="0.0.0.0", port=port, debug=True)
