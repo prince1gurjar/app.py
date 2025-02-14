6ad7944af7b647c57b55c5a9ba6713d38667eebf
prince1gurjar <49690988+prince1gurjar@users.noreply.github.com>
2025-02-14 21:05:21 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package list
+apt-get update 
+
+# Install Chrome
+apt-get install -y google-chrome-stable
+
+# Install ChromeDriver
+apt-get install -y chromium-chromedriver
+
+# Show versions (for debugging)
+google-chrome --version
+chromedriver --version
